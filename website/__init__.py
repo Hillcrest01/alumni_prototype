f7d208b4cac20cd1f5b779858d77026330fd0615
Hillcrest01 <peteromondi008@gmail.com>
2025-01-27 21:35:17 +0300

    fix: Fixed the LoginManager to ensure new users who sign up are redirected to login page

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
@@ -9,6 +11,10 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db'
 
     db.init_app(app)
+    login_manager.init_app(app)
+
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'Please log in to access this page'
 
     from .views import views
     from .auth import auth
@@ -26,3 +32,8 @@ def create_app():
         db.create_all()
         print("database created successfully")
     return app
+
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(int(user_id))
