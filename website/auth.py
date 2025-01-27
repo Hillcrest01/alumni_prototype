3a4d5b9e6af2a649aae8b1fa55cb8371280e9b63
Hillcrest01 <peteromondi008@gmail.com>
2025-01-27 23:05:25 +0300

    Add: Flash Messages can now be seen well styled

--- website/auth.py
+++ website/auth.py
@@ -17,24 +17,33 @@ def sign_up():
         email_address = request.form.get('email_address')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
+        year_of_study = int(year_of_study)
 
         new_user = User.query.filter_by(regno = regno).first()
         if new_user:
-            flash("User already exists! Log in or try a new email address")
+            flash("User already exists! Log in or try a new reg. number" , "error")
             print("Email Already exists")
         
         elif password1 != password2:
-            flash("passwords do not match")
+            flash("passwords do not match" , "error")
             print("Wrong Password")
-        
+
+
+        elif year_of_study < 1:
+                flash("Year of study should be at least one" , "error")
+                print("invalid year of study")
+
+        elif year_of_study > 4:
+                flash("Year of study should not be more than 4" , "error")
+                print("Invalid year of study")
         else:
-            new_user  = User(regno = regno , year_of_study = year_of_study, email_address = email_address, password_hash = generate_password_hash(password1) )
-            db.session.add(new_user)
-            db.session.commit()
-            flash("Account created successfully")
-            login_user(new_user, remember=True)
-            print("new user added to database")
-            return redirect(url_for('auth.login'))
+                new_user  = User(regno = regno , year_of_study = year_of_study, email_address = email_address, password_hash = generate_password_hash(password1) )
+                db.session.add(new_user)
+                db.session.commit()
+                flash("Account created successfully" , "success")
+                login_user(new_user, remember=True)
+                print("new user added to database")
+                return redirect(url_for('auth.login'))
 
 
 
@@ -52,16 +61,17 @@ def login():
         new_user = User.query.filter_by(regno = regno).first()
         if new_user:
             if check_password_hash(new_user.password_hash, password):
-                flash("login successful")
+                flash("login successful" , "success")
                 print("successful")
+                login_user(new_user, remember=True)
                 return redirect(url_for('views.home'))
             
             else:
-                flash('incorrect password')
+                flash('incorrect password' , "error")
                 print('login failed')
         
         else:
-            flash("no account linked with this reg number")
+            flash("no account linked with this reg number" , "error")
             print('no reg number linked')
 
     return render_template('login.html' , form = form , new_user = current_user)
@@ -71,8 +81,8 @@ def login():
 @login_required
 def logout():
     logout_user()
-    flash("you have successfully logged out")
-    return redirect(url_for('/'))
+    flash("you have successfully logged out", "success")
+    return redirect(url_for('views.home'))
 
 @auth.route('/profile')
 def profile():
