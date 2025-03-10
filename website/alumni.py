7df8e1dec2b45adb4b89d668f07db14723827ae6
Hillcrest01 <peteromondi008@gmail.com>
2025-03-10 12:33:40 +0300

    add:Aded the alumni interface

--- website/alumni.py
+++ website/alumni.py
@@ -1,8 +1,20 @@
 from flask import Blueprint, render_template
+from .models import Jobs, Events, User, Scholarships
 
 alumni = Blueprint('alumni' , __name__)
 
 
 @alumni.route('/alumni')
 def alumni_home():
-    return render_template('alumni.html')
+    jobs = Jobs.query.all()
+    events = Events.query.all()
+    members = User.query.count()
+    event = Events.query.count()
+    internship = Jobs.query.count()
+    scholarship = Scholarships.query.count()
+
+
+
+
+
+    return render_template('alumni.html' , jobs=jobs, events=events, members=members, event=event, scholarship=scholarship, internship=internship)
